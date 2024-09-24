e2e8a0dd122443cb6eccc28f57dc693daa1b8d79
Felipe Sousa <felipejosecsousa@gmail.com>
2024-09-23 22:49:47 -0300

    feat: add igw, route tables and enable vpc dns

--- main.tf
+++ main.tf
@@ -4,49 +4,39 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   
   tags = {
     Name = "main-vpc"
   }
 }
 
-resource "aws_subnet" "public_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   
   tags = {
-    Name = "public-subnet-1"
-  }
-}
-
-resource "aws_subnet" "public_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  
-  tags = {
-    Name = "public-subnet-2"
-  }
-}
-
-resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-  
-  tags = {
-    Name = "private-subnet-1"
-  }
-}
-
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-  
-  tags = {
-    Name = "private-subnet-2"
+    Name = "private-2"
   }
 }
 
@@ -70,31 +60,61 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc2" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc3" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
 resource "aws_db_instance" "default" {
-  identifier           = "my-postgres-db"
+  identifier           = "postgres-db"
   engine               = "postgres"
-  engine_version       = "13.7"
+  engine_version       = "14"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
   storage_type         = "gp2"
   username             = "dbadmin"
-  password             = "YourStrongPasswordHere"
+  password             = "teste123"
   db_name              = "mydb"
-  parameter_group_name = "default.postgres13"
+  parameter_group_name = "default.postgres14"
   skip_final_snapshot  = true
-  multi_az             = false 
+  publicly_accessible  = true
+
 
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.default.name
 
   tags = {
-    Name = "MyPostgresDB"
+    Name = "postgres"
   }
 }
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  subnet_ids = [aws_subnet.public.id, aws_subnet.private.id, aws_subnet.private2.id]
 
   tags = {
     Name = "My DB subnet group"
