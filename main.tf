22a77fb311f1aefc74073f22f15525abd3103e62
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-09-23 22:55:16 -0300

    feat: rds-dev version

--- main.tf
+++ main.tf
@@ -1,65 +1,35 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Change to your desired region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  
+resource "aws_db_instance" "postgres_instance" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "14"    # Specify desired PostgreSQL version
+  instance_class       = "db.t3.micro"
+  username             = "docker"   # Master username
+  password             = "dockerTech"   # Master password
+  parameter_group_name = "default.postgres14"
+  publicly_accessible  = true      # Make it publicly accessible
+  skip_final_snapshot  = true      # Skip final snapshot on delete
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]  # Link to security group
+
   tags = {
-    Name = "main-vpc"
+    Name = "Postgres-RDS"
   }
 }
 
-resource "aws_subnet" "public_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
-
-resource "aws_subnet" "public_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  
-  tags = {
-    Name = "public-subnet-2"
-  }
-}
-
-resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-  
-  tags = {
-    Name = "private-subnet-1"
-  }
-}
-
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-  
-  tags = {
-    Name = "private-subnet-2"
-  }
-}
-
+# Security group to allow inbound traffic on PostgreSQL port
 resource "aws_security_group" "rds_sg" {
-  name        = "rds-security-group"
-  description = "Security group for RDS instance"
-  vpc_id      = aws_vpc.main.id
+  name        = "rds_public_access_sg"
+  description = "Allow inbound access to PostgreSQL"
 
   ingress {
-    from_port   = 5432
+    from_port   = 5432  # PostgreSQL default port
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Open to the public (be careful with this in production)
   }
 
   egress {
@@ -70,37 +40,6 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
-resource "aws_db_instance" "default" {
-  identifier           = "my-postgres-db"
-  engine               = "postgres"
-  engine_version       = "13.7"
-  instance_class       = "db.t3.micro"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  username             = "dbadmin"
-  password             = "YourStrongPasswordHere"
-  db_name              = "mydb"
-  parameter_group_name = "default.postgres13"
-  skip_final_snapshot  = true
-  multi_az             = false 
-
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.default.name
-
-  tags = {
-    Name = "MyPostgresDB"
-  }
-}
-
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.default.endpoint
+output "db_endpoint" {
+  value = aws_db_instance.postgres_instance.endpoint
 }
